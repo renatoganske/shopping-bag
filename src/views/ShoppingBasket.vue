89ad883fb69a708db171da6d6568b238fe493954
Renato Ganske <renato.j@lyncas.net>
2023-08-04 16:33:33 -0300

    feat: lista de produtos no carrinho

--- src/views/ShoppingBasket.vue
+++ src/views/ShoppingBasket.vue
@@ -2,20 +2,32 @@
   <div class="basket">
     <div class="items">
 
-      <div class="item">
-        <div class="remove">Remover Produto</div>
-        <div class="photo"><img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt=""></div>
-        <div class="description">Mens Casual Premium Slim Fit T-Shirts </div>
-        <div class="price">
-          <span class="quantity-area">
-            <button disabled="">-</button>
-            <span class="quantity">1</span>
-            <button>+</button>
-          </span>
-          <span class="amount">R$ 22.30</span>
+      <template v-if="productsInBag.length">
+
+        <div v-for="(product, index) in productsInBag" :key="index" class="item">
+          <div class="remove" @click="this.$store.dispatch('removeFromBag', product.id)">Remover Produto</div>
+          <div class="photo">
+            <img :src="product.image" alt="">
+          </div>
+          <div class="description">
+            {{ product.title }}
+          </div>
+          <div class="price">
+            <span class="quantity-area">
+              <button :disabled="product.quantity <= 1" @click="product.quantity--">-</button>
+              <span class="quantity">{{ product.quantity }}</span>
+              <button @click="product.quantity++">+</button>
+            </span>
+            <span class="amount">R$ {{ (product.price * product.quantity).toFixed(2) }}</span>
+          </div>
         </div>
-      </div>
-      <div class="grand-total"> Total do pedido: R$ 22.30</div>
+        <div class="grand-total"> Total do pedido: R$ 22.30</div>
+
+      </template>
+
+      <template v-else>
+        <h4>Não há items no carrinho ainda</h4>
+      </template>
 
     </div>
   </div>
@@ -23,23 +35,28 @@
 
 <script>
 
+import { mapState } from 'vuex'
+
 export default {
   name: 'ShoppingBasket',
 
   methods: {
-   
+
   },
- 
+  computed: mapState([
+    'productsInBag'
+  ]),
 }
 </script>
 
 <style lang="scss">
-
 .basket {
-  padding: 60px 0;  
+  padding: 60px 0;
+
   .items {
     max-width: 800px;
     margin: auto;
+
     .item {
       display: flex;
       justify-content: space-between;
@@ -71,7 +88,7 @@ export default {
 
         .quantity {
 
-            margin: 0 4px;
+          margin: 0 4px;
         }
       }
 
@@ -97,15 +114,15 @@ export default {
         }
       }
     }
-      .grand-total {
-          font-size: 24px;
-          font-weight: bold;
-          text-align: right;
-          margin-top: 8px;
-      }
+
+    .grand-total {
+      font-size: 24px;
+      font-weight: bold;
+      text-align: right;
+      margin-top: 8px;
+    }
 
   }
 
 }
-
 </style>
